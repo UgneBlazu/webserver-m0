b7c289521a4505fde3275d8bfc90bf75e03ad962
UgneBlazu <u.blazukaite@gmail.com>
2024-08-26 10:00:25 +0200

    test4

--- www/public/index.php
+++ www/public/index.php
@@ -1,7 +1,26 @@
 <h1>Webbservern fungerar!</h1>
 <p><strong>Denna sida (index.php) skall bytas ut.</strong></p>
 
-<?php
-       echo"formulärer eller nåt";
-       
-?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8" />
+    <title>Quiz med Formulär och PHP</title>
+</head>
+<body>
+<h1>Quiz</h1>
+<form action="result.php" method="post">
+    <label>I vilket språk programmerar vi i på kursen?<br>    
+        <input type="radio" name="qOne" value="php">PHP<br>
+        <input type="radio" name="qOne" value="asp">ASP<br>
+        <input type="radio" name="qOne" value="jsp">JSP<br><br>
+    </label>
+    <label>Vilket språk är ett klientskriptspråk<br>  
+        <input type="radio" name="qTwo" value="jsp">JSP<br>
+        <input type="radio" name="qTwo" value="js">JavaScript<br>
+        <input type="radio" name="qTwo" value="basic">Basic<br><br>
+    </label>
+    <input type="submit" value="Skicka" />
+</form>
+</body>
+</html>
